de5fa35d10c5899f925c13d15cd3ad932505aaa9
Francis <franko200ng@gmail.com>
2023-11-08 13:00:15 +0100

    DronesTech modified file

---
+++ DronesTech/src/test/java/com/example/demo/DronesTechApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class DronesTechApplicationTests {
+
+
+	
+	@Test
+	void contextLoads(){
+
+	}
+	
+
+
+}
